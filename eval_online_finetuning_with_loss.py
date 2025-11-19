3144eec227e6299f2c6fb04e117ed7db3664ca81
alexdenker <adenker@uni-bremen.de>
2025-11-19 20:02:34 +0000

    naive online finetuning

---
+++ eval_online_finetuning_with_loss.py
@@ -0,0 +1,282 @@
+
+
+"""
+Implementation of the VarGrad loss. Change the direction of the SDE: going from 0 -> T
+with x0 ~ N(0, I) and xT ~ data distribution
+ 
+"""
+
+import torch
+import numpy as np 
+import matplotlib.pyplot as plt 
+import os 
+import yaml 
+
+
+from torchvision.utils import make_grid, save_image
+from tqdm import tqdm 
+
+from src import UNetModel, VPSDE, ScaleModel, Superresolution, flowers102
+
+device = "cuda"
+
+base_path = "model_weights"
+
+with open(os.path.join(base_path, "report.yaml"), "r") as f:
+    cfg_dict = yaml.safe_load(f)
+
+#cond_base_path = "cond_weights_var_grad"
+cond_base_path = "/home/alexdenker/projects/OnlineFinetuning/self_supervised_weights_rebuttal/iterative_flowers_targetimg=3_lam=1.0_kl=0.0_initScale=0.002/9sptamwc"
+#cond_base_path = "cond_weights_rtb"
+#cond_base_path = "cond_weights_adjoint_sde"
+cond_base_path = "cond_weights_dpok"
+
+
+with open(os.path.join(cond_base_path, "report.yaml"), "r") as f:
+    cfg = yaml.safe_load(f)
+
+sde = VPSDE(beta_min=cfg_dict["diffusion"]["beta_min"], 
+            beta_max=cfg_dict["diffusion"]["beta_max"]
+            )
+
+model = UNetModel(
+           marginal_prob_std=sde.marginal_prob_std,
+            model_channels=cfg_dict["model"]["model_channels"],
+            max_period=cfg_dict["model"]["max_period"],
+            num_res_blocks=cfg_dict["model"]["num_res_blocks"],
+            in_channels=cfg_dict["model"]["in_channels"],
+            out_channels=cfg_dict["model"]["out_channels"],
+            attention_resolutions=cfg_dict["model"]["attention_resolutions"],
+            channel_mult=cfg_dict["model"]["channel_mult"])
+model.load_state_dict(torch.load(os.path.join(base_path,"model.pt")))
+model.to(device)
+model.eval() 
+
+val_dataset = flowers102(root="dataset/flowers",split="val")
+
+x_gt = val_dataset[cfg["val_img_idx"]][0].unsqueeze(0).to("cuda")
+
+lkhd = Superresolution(scale=2, sigma_y=0.05, device="cuda")
+
+torch.manual_seed(123)
+y_noise = lkhd.sample(x_gt)
+
+y_gt = lkhd.A(x_gt)
+data_consistency_gt = torch.sum((y_gt - y_noise)**2)
+
+Aty = lkhd.A_adjoint(y_noise)
+noise_level = 0.05 
+
+class CondSDE(torch.nn.Module):
+    def __init__(self, model, sde, y_noise):
+        super().__init__()
+
+        self.model = model 
+        self.sde = sde 
+        self.cond_model = UNetModel(
+            marginal_prob_std=sde.marginal_prob_std,
+            model_channels=cfg["model"]["model_channels"],
+            max_period=cfg["model"]["max_period"],
+            num_res_blocks=cfg["model"]["num_res_blocks"],
+            in_channels=cfg["model"]["in_channels"],
+            out_channels=cfg["model"]["out_channels"],
+            attention_resolutions=cfg["model"]["attention_resolutions"],
+            channel_mult=cfg["model"]["channel_mult"])
+        self.cond_model.load_state_dict(torch.load(os.path.join(cond_base_path,"cond_model.pt")))
+
+        self.cond_model.to("cuda")
+        self.cond_model.train() 
+
+        self.time_model = ScaleModel(time_embedding_dim=cfg["time_embedding_dim"],
+                                    max_period=cfg_dict["model"]["max_period"], 
+                                    dim_out=1,
+                                    init_scale=cfg["init_scale"])
+        self.time_model.load_state_dict(torch.load(os.path.join(cond_base_path,"time_model.pt")))
+
+        self.time_model.to(device)
+
+        self.y_noise = y_noise
+    
+    def forward(self, ts, x0):
+        """
+        Implement EM solver
+
+        """
+        x_t = [x0] 
+        #print("time steps: ", ts)
+        kldiv_term1 = torch.zeros(1).to(x0.device)
+        kldiv_term2 = torch.zeros(1).to(x0.device)
+        kldiv_term3 = torch.zeros(1).to(x0.device)
+
+        for t0, t1 in zip(ts[:-1], ts[1:]):
+            #print(t0, t1)
+            dt = t1 - t0 
+            #print(dt)
+            dW = torch.randn_like(x0) * torch.sqrt(dt)
+            ones_vec = torch.ones(x0.shape[0], device=x_gt.device)
+            t = ones_vec * t0
+            #print("Time step: ", t0, " to model: ", 1-t0)
+            with torch.no_grad():
+                s_pretrained = self.model(x_t[-1], 1 - t)
+
+            cond = torch.repeat_interleave(self.y_noise,  dim=0, repeats=x0.shape[0])
+
+            with torch.no_grad():
+                if cfg["use_tweedie"]:
+                    marginal_prob_mean = self.sde.marginal_prob_mean_scale(1-t)
+                    marginal_prob_std = self.sde.marginal_prob_std(1-t)
+
+                    x0hat = (x_t[-1] + marginal_prob_std[:,None,None,None]**2*s_pretrained)/marginal_prob_mean[:,None,None,None]
+                    log_grad = -lkhd.log_likelihood_grad(x0hat, cond) #forward_op.trafo_adjoint(forward_op.trafo(x0hat) - cond)
+
+                else:
+                    log_grad = -lkhd.log_likelihood_grad(xt[-1], cond) #forward_op.trafo_adjoint(forward_op.trafo(x_t[-1]) - cond)
+            
+            log_grad_scaling = self.time_model(1. - t)[:, None, None]
+            #print(log_grad_scaling.shape) #.view(t.shape[0], 1, 28, 28)
+            h_trans = self.cond_model(torch.cat([log_grad, x_t[-1]], dim=1), 1 - t) 
+            h_trans = h_trans - log_grad_scaling*log_grad 
+
+
+
+            s = s_pretrained + h_trans
+            drift, diffusion = self.sde.sde(x_t[-1], 1 - t) # diffusion = sqrt(beta)
+            # drift = - 0.5 beta x
+            f_t =  - drift + diffusion[:, None, None, None].pow(2)*s
+
+            #print("Base model norm: ", torch.norm(s_pretrained))
+            #print("Cond model norm: ", torch.norm(h_trans))
+            #print("Diffusion: ", diffusion.pow(2).shape)
+
+            f_sq = (h_trans ** 2).sum(dim=(1,2,3))
+            g_f = (h_trans * h_trans.detach()).sum(dim=(1,2,3))
+            f_w = (h_trans * dW).sum(dim=(1,2,3))
+
+            #print("f_sq: ", f_sq.shape, g_f.shape, f_w.shape)
+
+            kldiv_term1 = kldiv_term1 - 0.5 * f_sq * diffusion.pow(2) * dt
+            kldiv_term2 = kldiv_term2 + diffusion.pow(2) * dt * g_f 
+            kldiv_term3 = kldiv_term3 + diffusion * f_w 
+
+            g_t = diffusion[:, None, None, None]
+            x_new = x_t[-1] + f_t * dt + g_t * dW
+            x_t.append(x_new.detach())
+            
+        return x_t, kldiv_term1, kldiv_term2, kldiv_term3
+
+
+sde_model = CondSDE(model=model, sde=sde, y_noise=y_noise)
+sde_model.cond_model.eval()
+sde_model.time_model.eval()
+sde_model.eval()
+
+t_size = 200
+
+batch_size = 64
+with torch.no_grad():
+    ts_fine = np.sqrt(np.linspace(0, (1. - 1e-3)**2, t_size))
+
+    #ts_fine = np.linspace(0, 1-1e-3, t_size) #np.sqrt(np.linspace(0, (1. - 1e-3)**2, t_size))
+    ts_fine = torch.from_numpy(ts_fine).to(device)
+
+    x0 = torch.randn((batch_size, 3, 64, 64)).to(device)
+
+    xt, kldiv_term1, _, _ = sde_model.forward(ts_fine, x0)
+    xt = xt[-1]
+
+    data_fit = torch.sum((lkhd.A(xt) - y_noise)**2, dim=(1,2,3))
+    loss_data = 1/2*1/noise_level**2*data_fit
+    print(loss_data.shape, kldiv_term1.shape)
+    print("loss_data: ", loss_data.mean().item())
+    print("kldiv_term: ", kldiv_term1.mean().item())
+    loss = (loss_data - kldiv_term1).mean()
+
+
+print("SOC loss on samples: ", loss.item())
+
+xt = (xt + 1.) / 2.
+xt = torch.clamp(xt, 0, 1)
+
+xt_grid = make_grid(xt, nrow=4)
+
+
+save_image(xt_grid, "online_flower_samples.png")
+save_image((x_gt[0] + 1.)/2., "online_flower_gt.png")
+save_image((y_noise[0] + 1.)/2., "online_flower_y.png")
+
+
+print(xt_grid.shape)
+
+plt.figure()
+plt.imshow(xt_grid.permute(1,2,0).cpu().numpy())
+plt.axis("off")
+plt.show()
+
+
+num_samples = 1000
+sampl_per_batch = 20 
+
+
+sample_list = []
+
+for i in tqdm(range(num_samples // sampl_per_batch)):
+
+    with torch.no_grad():
+        ts_fine = np.sqrt(np.linspace(0, (1. - 1e-3)**2, t_size))
+        ts_fine = torch.from_numpy(ts_fine).to(device)
+
+        x0 = torch.randn((sampl_per_batch, 3, 64, 64)).to(device)
+
+        xt = sde_model.forward(ts_fine, x0)
+        xt = torch.clamp(xt, -1, 1)
+        sample_list.append(xt)
+
+
+samples = torch.cat(sample_list)
+print(samples.shape)
+
+
+
+y_sim = lkhd.A(samples)
+
+print(y_sim.shape, y_noise.shape)
+data_consistency = torch.sum((y_sim - y_noise)**2, dim=(1,2,3))
+
+print("data consistency: ", data_consistency.shape)
+
+#fig, (ax1) = plt.subplots(1,1, figsize=(16,4))
+
+
+#ax1.hist(data_consistency.cpu().numpy().ravel(), bins="auto", alpha=0.75)
+#ax1.set_title("|| A(x) - y_noise||")
+#ax1.vlines(data_consistency_gt.cpu().numpy(), 0, 10, label="|| A(x_gt) - y_noise||", colors='r')
+#ax1.legend()
+
+#plt.show()
+
+
+fig, axes = plt.subplots(6,2)
+
+for idx, ax in enumerate(axes.ravel()):
+
+    im = ax.imshow((samples[idx].permute(1,2,0).cpu() + 1.)/2.)
+    ax.axis("off")
+    #ax1.set_title("Ground truth")
+
+plt.show()
+
+
+mean_sample = torch.mean(samples, axis=0)
+
+
+fig, (ax1, ax2) = plt.subplots(1,2)
+
+im = ax1.imshow((x_gt[0].permute(1,2,0).cpu() + 1.)/2., cmap="gray")
+ax1.axis("off")
+ax1.set_title("Ground truth")
+
+im = ax2.imshow((mean_sample.permute(1,2,0).cpu() + 1.)/2., cmap="gray")
+ax2.axis("off")
+ax2.set_title("Mean Sample")
+
+plt.show()
--- online_finetuning.py
+++ online_finetuning.py
@@ -1,64 +1,67 @@
-
-
 """
-Implementation of the VarGrad loss. Change the direction of the SDE: going from 0 -> T
-with x0 ~ N(0, I) and xT ~ data distribution
- 
+Implementation of online finetuning using the naive approach:
+
+We backpropagate through the entire SDE trajectory to compute gradients.
+Note, that we have to use:
+    - gradient checkpointing 
+    - gradient accumulation 
+to fit the model in GPU memory.
+
+This makes the training very slow, I get about 1 iteration per 30s. 
+
 """
 
 import torch
 import numpy as np 
-import matplotlib.pyplot as plt 
 import os 
 import yaml 
-import time 
-import math 
-
-import torch 
-import torch.nn as nn 
-
 from tqdm import tqdm 
-from skimage.metrics import peak_signal_noise_ratio
+import time 
 
 from src import UNetModel, VPSDE, ScaleModel, Superresolution, flowers102
 
+
 cfg = {
-    'model': 
+    'model': # these are parameters for the h-transform network and time-scaling network
     { "in_channels": 6,
      "out_channels": 3,
      "model_channels": 32,
      "channel_mult": [1,2],
      "num_res_blocks": 1,
      "attention_resolutions": [],
-     "max_period": 0.005},
-    'batch_size': 12,
-    'time_steps_min': 200,
-    'time_steps_max': 200,
-    'lr': 1e-4,
-    'num_steps':  300,
-    'log_img_freq': 10,
+     "max_period": 0.005,
+     "time_embedding_dim": 256,
+     "init_scale": 5e-3},
+    'batch_size': 2, 
+    "grad_accum_steps": 8,
+    "num_time_steps": 200,
+    'lr': 1e-5,
+    'num_steps':  1000,
     'use_tweedie': True,
     'clip_gradient': True,
-    'clip_value': 1.0, 
-    "batch_size_val": 12,
-    "init_scale": 4e-3,
-    "time_embedding_dim": 256,
-    'val_img_idx': 0,
+    'clip_value': 1.0,     
+    'val_img_idx': 3, 
     'device': "cuda",
-        "t_size_val": 800,
-
 }
 
 device = cfg["device"]
 
 base_path = "model_weights"
 
-with open(os.path.join("cond_weights", "report.yaml"), "w") as file:
+cond_pretrain_path = "cond_weights_rtb"
+
+# add time stamp to save directory
+cur_time = time.strftime("%Y%m%d-%H%M%S")
+save_dir = os.path.join("online_finetune_naive", cur_time)
+os.makedirs(save_dir, exist_ok=True)
+
+with open(os.path.join(save_dir, "report.yaml"), "w") as file:
     yaml.dump(cfg, file)
 
 
 with open(os.path.join(base_path, "report.yaml"), "r") as f:
     cfg_dict = yaml.safe_load(f)
+
 
 sde = VPSDE(beta_min=cfg_dict["diffusion"]["beta_min"], 
             beta_max=cfg_dict["diffusion"]["beta_max"]
@@ -72,22 +75,22 @@ model = UNetModel(
             in_channels=cfg_dict["model"]["in_channels"],
             out_channels=cfg_dict["model"]["out_channels"],
             attention_resolutions=cfg_dict["model"]["attention_resolutions"],
-            channel_mult=cfg_dict["model"]["channel_mult"])
+            channel_mult=cfg_dict["model"]["channel_mult"],
+            use_checkpoint=True)
 model.load_state_dict(torch.load(os.path.join(base_path,"model.pt")))
 model.to(device)
 model.eval() 
 
 val_dataset = flowers102(root="dataset/flowers",split="val")
 
-x_gt = val_dataset[cfg["val_img_idx"]][0].unsqueeze(0).to("cuda")
+x_gt = val_dataset[cfg["val_img_idx"]][0].unsqueeze(0).to(device)
 
+noise_level = 0.05
+lkhd = Superresolution(scale=2, sigma_y=noise_level, device=device)
 
-lkhd = Superresolution(scale=4, sigma_y=0.05, device="cuda")
-
+torch.manual_seed(123)
 y_noise = lkhd.sample(x_gt)
 
-Aty = lkhd.A_adjoint(y_noise)
-noise_level = 0.05 
 
 class CondSDE(torch.nn.Module):
     def __init__(self, model, sde, y_noise):
@@ -103,181 +106,146 @@ class CondSDE(torch.nn.Module):
             in_channels=cfg["model"]["in_channels"],
             out_channels=cfg["model"]["out_channels"],
             attention_resolutions=cfg["model"]["attention_resolutions"],
-            channel_mult=cfg["model"]["channel_mult"])
+            channel_mult=cfg["model"]["channel_mult"],
+            use_checkpoint=True
+        )
+        if cond_pretrain_path is not None:
+            self.cond_model.load_state_dict(torch.load(os.path.join(cond_pretrain_path,"cond_model.pt")))
         self.cond_model.to("cuda")
         self.cond_model.train() 
 
-        self.time_model = ScaleModel(time_embedding_dim=cfg["time_embedding_dim"],
+        self.time_model = ScaleModel(time_embedding_dim=cfg["model"]["time_embedding_dim"],
                                     max_period=cfg_dict["model"]["max_period"], 
                                     dim_out=1,
-                                    init_scale=cfg["init_scale"])
+                                    init_scale=cfg["model"]["init_scale"])
+        if cond_pretrain_path is not None:
+            self.time_model.load_state_dict(torch.load(os.path.join(cond_pretrain_path,"time_model.pt")))
         self.time_model.to(device)
 
         self.y_noise = y_noise
     
+    def h_transform(self, x_t, t, score_pretrained, cond):
+        """
+        This implement our gradient-informed parametrisation of the h-transform, given has 
+            NN(x_t, t) = NN1(x_t, t) - scaling(t) * grad_x0hat log p(y|x0hat(x_t))
+        
+        where x0hat(x_t) is the Tweedie estimate of x0 given x_t and the pretrained score model
+
+        In particular, we treat "grad_x0hat log p(y|x0hat(x_t))" as a constant, 
+        detached from the computational graph.
+        """
+
+        if cfg["use_tweedie"]:
+            marginal_prob_mean = self.sde.marginal_prob_mean_scale(1-t)
+            marginal_prob_std = self.sde.marginal_prob_std(1-t)
+
+            x0hat = (x_t + marginal_prob_std[:,None,None,None]**2*score_pretrained)/marginal_prob_mean[:,None,None,None]
+            log_grad = -lkhd.log_likelihood_grad(x0hat, cond).detach()
+
+        else:
+            log_grad = -lkhd.log_likelihood_grad(x_t, cond).detach()
+
+        log_grad_scaling = self.time_model(1. - t)[:, None, None]
+        h_trans = self.cond_model(torch.cat([log_grad, x_t], dim=1), 1 - t) 
+        h_trans = h_trans - log_grad_scaling*log_grad
+
+        return h_trans 
+    
     def forward(self, ts, x0):
         """
-        Implement EM solver
+        Implement EM solver. 
+        We have the forward SDE as:
+            dx = f(x,t)dt + g(t)dW_t
+        and thus the reverse-time SDE is:
+            dx = [f(x,t) - g(t)^2 * s(x,t) ] dt + g(t)dW_t
+        where s(x,t) is the score function and the time runs from [T,0].
+        
+        However, we implement the reverse SDE in forward time, i.e., from t=0 to t=T, so we have:
+            dx = [-f(x,1-t) + g(1-t)^2 * s(x,1-t) ] dt + g(1-t)dW_t
+        
+        Leading to the Euler-Maruyama update as:
+            x_{t+dt} = x_t + [-f(x,1-t) + g(1-t)^2 * s(x,1-t) ] dt + g(1-t) * dt.sqrt() * eps
+            with eps ~ N(0,I)
+
+        The KL divergence term is computed as:
+            KL = E[ int_0^T 0.5 * g(1-t)^2 * ||  h(x,1-t) ||^2 dt ]
+        where h(x,1-t) is the h-transform.
 
         """
-        x_t = [x0] 
-        #print("time steps: ", ts)
-        kldiv_term1 = torch.zeros(1).to(x0.device)
-        kldiv_term2 = torch.zeros(1).to(x0.device)
-        kldiv_term3 = torch.zeros(1).to(x0.device)
 
-        t0s = np.random.choice(ts[:-1].cpu().numpy(), size=int(0.2*len(ts[:-1])), replace=False)
+        x_t = torch.clone(x0)
+
+        kldiv_term = torch.zeros(x0.shape[0]).to(x0.device)        
+        
+        cond = torch.repeat_interleave(self.y_noise, dim=0, repeats=x0.shape[0])
+
         for t0, t1 in zip(ts[:-1], ts[1:]):
-            #print(t0, t1)
             dt = t1 - t0 
-            #print(dt)
+
             dW = torch.randn_like(x0) * torch.sqrt(dt)
             ones_vec = torch.ones(x0.shape[0], device=x_gt.device)
             t = ones_vec * t0
-            #print("Time step: ", t0, " to model: ", 1-t0)
-            with torch.no_grad():
-                s_pretrained = self.model(x_t[-1], 1 - t)
-
-            cond = torch.repeat_interleave(self.y_noise,  dim=0, repeats=x0.shape[0])
-
-            with torch.no_grad():
-                if cfg["use_tweedie"]:
-                    marginal_prob_mean = self.sde.marginal_prob_mean_scale(1-t)
-                    marginal_prob_std = self.sde.marginal_prob_std(1-t)
-
-                    x0hat = (x_t[-1] + marginal_prob_std[:,None,None,None]**2*s_pretrained)/marginal_prob_mean[:,None,None,None]
-                    log_grad = -lkhd.log_likelihood_grad(x0hat, cond) #forward_op.trafo_adjoint(forward_op.trafo(x0hat) - cond)
-
-                else:
-                    log_grad = -lkhd.log_likelihood_grad(xt[-1], cond) #forward_op.trafo_adjoint(forward_op.trafo(x_t[-1]) - cond)
             
-            log_grad_scaling = self.time_model(1. - t)[:, None, None]
-            #print(log_grad_scaling.shape) #.view(t.shape[0], 1, 28, 28)
-            h_trans = self.cond_model(torch.cat([log_grad, x_t[-1]], dim=1), 1 - t) 
-            h_trans = h_trans - log_grad_scaling*log_grad 
+            s_pretrained = self.model(x_t, 1 - t)
+
+            h_trans = self.h_transform(x_t, t, s_pretrained, cond)
 
             s = s_pretrained + h_trans
-            drift, diffusion = self.sde.sde(x_t[-1], 1 - t) # diffusion = sqrt(beta)
-            # drift = - 0.5 beta x
+            drift, diffusion = self.sde.sde(x_t, 1 - t) # diffusion = sqrt(beta)
+
             f_t =  - drift + diffusion[:, None, None, None].pow(2)*s
 
-
             f_sq = (h_trans ** 2).sum(dim=(1,2,3))
-            g_f = (h_trans * h_trans.detach()).sum(dim=(1,2,3))
-            f_w = (h_trans * dW).sum(dim=(1,2,3))
 
-            if t0.item() in t0s:
-                kldiv_term1 = kldiv_term1 - 0.5 * f_sq * diffusion.pow(2) * dt * 5 # only tak 20% of time steps
-                kldiv_term2 = kldiv_term2 + diffusion.pow(2) * dt * g_f * 5
-                kldiv_term3 = kldiv_term3 + diffusion * f_w
+            kldiv_term = kldiv_term - 0.5 * f_sq * diffusion.pow(2) * dt
 
             g_t = diffusion[:, None, None, None]
-            x_new = x_t[-1] + f_t * dt + g_t * dW
-            x_t.append(x_new.detach())
-            
-        return x_t, kldiv_term1.detach(), kldiv_term2.detach(), kldiv_term3
+            x_t = x_t + f_t * dt + g_t * dW
 
+        return x_t, kldiv_term
 
 
 batch_size = cfg["batch_size"]
 sde_model = CondSDE(model=model, sde=sde, y_noise=y_noise)
+
 #t_size = cfg["time_steps"]
 optimizer = torch.optim.Adam(list(sde_model.cond_model.parameters()) + list(sde_model.time_model.parameters()), lr=cfg["lr"])
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.9)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=cfg["num_steps"], eta_min=cfg["lr"]/1000.)
 
-x_target = x_gt.repeat(batch_size, 1, 1, 1)
+sde_model.cond_model.train()
 
-fig, (ax1, ax2) = plt.subplots(1,2, figsize=(14,6))
+cond = torch.repeat_interleave(y_noise,  dim=0, repeats=batch_size)
 
-ax1.set_title("Ground truth")
-ax1.imshow((x_gt[0].permute(1,2,0).cpu() + 1.)/2., cmap="gray")
-ax1.axis("off")
+with tqdm(total=cfg["num_steps"]) as pbar:
+    for step in range(cfg["num_steps"]):
+        optimizer.zero_grad()
 
-ax2.set_title("y")
-ax2.imshow((y_noise[0].permute(1,2,0).cpu().numpy() + 1.)/2., cmap="gray")
-ax2.axis("off")
+        for _ in range(cfg["grad_accum_steps"]):
 
-plt.show()
+            x0 = torch.randn((batch_size, 3, 64, 64), device=device)
+            
+            
+            ts = np.sqrt(np.linspace(0, (1. - 1e-3)**2, cfg["num_time_steps"] ) )
+            ts = torch.from_numpy(ts).to(device)
 
-for step in tqdm(range(cfg["num_steps"])):
-    sde_model.cond_model.train()
-    optimizer.zero_grad()
+            xt, kldiv_term = sde_model.forward(ts, x0)
 
-    x0 = torch.randn((batch_size, 3, 64, 64)).to("cuda")
-    if cfg["time_steps_min"] == cfg["time_steps_max"]:
-        t_size = cfg["time_steps_min"]
-    else:
-        t_size = np.random.randint(cfg["time_steps_min"], cfg["time_steps_max"])
-    ts = np.sqrt(np.linspace(0, (1. - 1e-3)**2, t_size))
-    ts = torch.from_numpy(ts).to(device)
-    time_start = time.time()
-    
-    xt, kldiv_term1, kldiv_term2, kldiv_term3 = sde_model.forward(ts, x0)
+            data_fit = torch.sum((lkhd.A(xt) - cond)**2, dim=(1,2,3))
+            loss_data = 0.5*1/noise_level**2*data_fit
 
-    cond = torch.repeat_interleave(y_noise,  dim=0, repeats=batch_size)
-    data_fit = torch.sum((lkhd.A(xt[-1]) - cond)**2, dim=(1,2,3))
-    loss_data = 1/2*1/noise_level**2*data_fit
-    loss_kl = kldiv_term1 + kldiv_term2 + kldiv_term3
-    loss = loss_data + kldiv_term1 + kldiv_term2 + kldiv_term3
-
-    # filter out to large loss values 
-    loss_filtered = loss[loss < 50000]
-    if len(loss_filtered) > int(0.3*batch_size):
-        loss_filtered = torch.var(loss_filtered)
-
-        original_loss = (loss_data[loss < 50000] - kldiv_term1[loss < 50000]).mean()
-        print("Fine-tuning loss: ", original_loss.item())
-        mse_target = torch.mean((xt[-1] - x_target)**2)
-        psnrs = [] 
-        for i in range(xt[-1].shape[0]):
-            psnr = peak_signal_noise_ratio(x_target.detach().cpu().numpy()[i], xt[-1].detach().cpu().numpy()[i], data_range=2.)
-            psnrs.append(psnr)
-
-        time_start = time.time() 
-        loss_filtered.backward()
+            soc_loss = (loss_data - kldiv_term).mean() / cfg["grad_accum_steps"]
+            soc_loss.backward()
 
         if cfg['clip_gradient']:
             torch.nn.utils.clip_grad_norm_(sde_model.cond_model.parameters(), cfg['clip_value'])
             torch.nn.utils.clip_grad_norm_(sde_model.time_model.parameters(), cfg['clip_value'])
-        
+            
         optimizer.step()
         scheduler.step()
         
-    else:
-        print("Too many loss values were filtered out. Draw new samples")
+        pbar.set_description(f"SOC loss: {soc_loss.item()*cfg["grad_accum_steps"]}")
+        pbar.update(1)
 
-    torch.save(sde_model.cond_model.state_dict(), "cond_weights/cond_model.pt")
-    torch.save(sde_model.time_model.state_dict(), "cond_weights/time_model.pt")
-
-    if step % cfg["log_img_freq"] == 0 and step > 0:
         
-        val_log_dir = {}
-
-        sde_model.cond_model.eval()
-
-        x_target_val = x_gt.repeat(cfg["batch_size_val"], 1, 1, 1)
-        with torch.no_grad():
-            ts_fine = np.sqrt(np.linspace(0, (1. - 1e-3)**2, cfg["t_size_val"]))
-            ts_fine = torch.from_numpy(ts_fine).to(device)
-
-            x0 = torch.randn((cfg["batch_size_val"], 3, 64, 64)).to(device)
-            xt, _, _, _ = sde_model.forward(ts_fine, x0)
-
-        mse_target = torch.mean((xt[-1] - x_target_val)**2)
-
-        fig, axes = plt.subplots(1,8, figsize=(13,6))
-        for idx, ax in enumerate(axes.ravel()):
-            if idx == 0:
-                ax.imshow((x_gt[0].permute(1,2,0).cpu().numpy() + 1.)/2.)
-                ax.set_title("GT")
-            elif idx == 1:
-                ax.imshow((y_noise[0].permute(1,2,0).cpu().numpy() + 1.)/2.)
-                ax.set_title("y (4x ds)")
-            else:
-                ax.imshow((xt[-1][idx-1].permute(1,2,0).detach().cpu().numpy() + 1.)/2.)
-                ax.set_title(f"Sample {idx-1}")
-            ax.axis("off")
-
-        plt.show()
-    
-        
+        torch.save(sde_model.cond_model.state_dict(), os.path.join(save_dir, "cond_model.pt"))
+        torch.save(sde_model.time_model.state_dict(), os.path.join(save_dir, "time_model.pt"))
